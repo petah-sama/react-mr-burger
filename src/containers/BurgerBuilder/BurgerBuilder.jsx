9942702c3e168f33897c2d939463f3d0a635f9bd
Pedro Batista <pedrobatista.420@gmail.com>
2021-04-13 16:01:03 +0100

    added state to burger builder to check number of ingredients the burger has. burger now outputs ingredients dynamically

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,10 +1,21 @@
 import React, { Component, Fragment } from 'react';
 
+import Burger from '../../components/Burger/Burger';
+
 class BurgerBuilder extends Component {
+    state = {
+        ingredients : {
+            salad: 1,
+            cheese: 2,
+            meat: 1,
+            bacon: 1
+        }
+    }
+
     render () {
         return (
             <Fragment>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Fragment>
         );
