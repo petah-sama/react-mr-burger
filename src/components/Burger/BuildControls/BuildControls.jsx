6b274e497ec129e21c8f13bf78cadd0686560dfc
Pedro Batista <pedrobatista.420@gmail.com>
2021-04-13 16:27:01 +0100

    added BuildControls to BurgerBuilder

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+import BuildControl from './BuildControl/BuildControl';
  
-import classes from './BuildControls.module.css'
+import classes from './BuildControls.module.css';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Meat', type: 'meat' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' }
+];
  
 const buildControls = (props) => (
-    
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => {
+            return <BuildControl key={ctrl.label} label={ctrl.label} />
+        })}
+    </div>
 );
  
 export default buildControls;
